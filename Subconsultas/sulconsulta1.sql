88b87b209f31c5db6f8d64c981315b025e3e318f
Carlos Moscol <48760202+CarlosMoscol@users.noreply.github.com>
2023-08-21 17:25:53 -0500

    Update sulconsulta1.sql

--- Subconsultas/sulconsulta1.sql
+++ Subconsultas/sulconsulta1.sql
@@ -4,9 +4,20 @@ SELECT ProductID AS PID,
 	   (SELECT Price FROM Products WHERE OrderDetails.ProductID = ProductID) AS Precio
 	   FROM OrderDetails
 
-
+-- Producto mayor recaudado
 SELECT ProductID, sum(Quantity) AS TOTAL_VENDIDO,
 	   (SELECT Price FROM Products WHERE ProductID = OrderDetails.ProductID) AS Price,
-	   (sum(Quantity) * (SELECT Price FROM Products WHERE ProductID = OrderDetails.ProductID)) AS TOTAL_RECAUDO
+	   (SELECT ProductName FROM Products WHERE ProductID = OrderDetails.ProductID) AS Name,
+	   round((sum(Quantity) * (SELECT Price FROM Products WHERE ProductID = OrderDetails.ProductID))) AS Total_Recaudado
 FROM OrderDetails
 GROUP BY ProductID
+ORDER BY TOTAL_RECAUDADO DESC
+
+-- 
+SELECT ProductID, sum(Quantity) AS TOTAL_VENDIDO,
+	   (SELECT ProductName FROM Products WHERE ProductID = OrderDetails.ProductID) AS Name,
+	   round((sum(Quantity) * (SELECT Price FROM Products WHERE ProductID = OrderDetails.ProductID))) AS Total_Recaudado
+FROM OrderDetails
+WHERE (SELECT Price FROM Products WHERE ProductID = OrderDetails.ProductID) > 40
+GROUP BY ProductID
+ORDER BY TOTAL_RECAUDADO
